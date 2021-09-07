7d3019a9f9c472a5f2f503b676a5827a2b55cbe1
Kstarreveld <70197481+Kstarreveld@users.noreply.github.com>
2021-09-07 11:18:22 +0200

    Add files via upload
    API

---
+++ user.php
@@ -0,0 +1,52 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json;  charset=UTF-8');
+function cors() {
+    
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+       
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+        
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    
+        exit(0);
+    }
+}
+
+// Retrieve the posted data
+$data = json_decode(file_get_contents("php://input"));
+
+
+$con = mysqli_connect("localhost","root","","loginapp");
+$result = mysqli_query($con, "SELECT * FROM users WHERE name='$data->user'");
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+$returndata = [ 
+        'status' => 200,
+        'user' => $row
+];
+cors();
+
+// Transfer data to json
+echo json_encode($returndata);
+
+
+?>
